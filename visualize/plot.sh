47529e8cf43bb99b394ab2d49b5c16a129d44b64
Alireza Azimi <sazimi@ualberta.ca>
2025-08-26 13:25:40 -0600

    fix

--- visualize/plot.sh
+++ visualize/plot.sh
@@ -18,13 +18,26 @@
 
 python tb_plot.py   --logdir ../logs --title "Average Reward" --yaxis "Average Reward"  --out figs_pub/img_prioception   --tags "episode/sum_reward"   --groups "\
 Img+Prioception (Cartesian Increment)=**/PandaPickCubeCartesianModified-cartesian_increment-position-_prop-*;
-Img+Prioception (Velocity Increment)=**/PandaPickCubeCartesianModified-joint_increment-velocity-_prop-*;
+Img+Prioception (Velocity)=**/PandaPickCubeCartesianModified-joint-velocity-_prop-*;
 Img+Prioception (Position Increment)=**/PandaPickCubeCartesianModified-joint_increment-position-_prop-*;
-Img+Prioception (Torque Increment)=**/PandaPickCubeCartesianModified-joint_increment-torque-_prop-*"   --smoothing 0.9
+Img+Prioception (Torque)=**/PandaPickCubeCartesianModified-joint-torque-_prop-*"   --smoothing 0.9
 
-python tb_plot.py   --logdir ../logs --title "Success Rate" --yaxis "Success"  --out figs_pub/img_prioception   --tags "episode/reward/success"   --groups "\
+python tb_plot.py   --logdir ../logs --title "Success Rate" --yaxis "Success"  --out figs_pub/img_prioception   --tags "episode/success"   --groups "\
 Img+Prioception (Cartesian Increment)=**/PandaPickCubeCartesianModified-cartesian_increment-position-_prop-*;
-Img+Prioception (Velocity Increment)=**/PandaPickCubeCartesianModified-joint_increment-velocity-_prop-*;
+Img+Prioception (Velocity)=**/PandaPickCubeCartesianModified-joint-velocity-_prop-*;
 Img+Prioception (Position Increment)=**/PandaPickCubeCartesianModified-joint_increment-position-_prop-*;
-Img+Prioception (Torque Increment)=**/PandaPickCubeCartesianModified-joint_increment-torque-_prop-*"   --smoothing 0.9
+Img+Prioception (Torque)=**/PandaPickCubeCartesianModified-joint-torque-_prop-*"   --smoothing 0.9
+
+
+python tb_plot.py   --logdir ../logs --title "Average Reward" --yaxis "Average Reward"  --out figs_pub/img   --tags "episode/sum_reward"   --groups "\
+Img (Cartesian Increment)=**/PandaPickCubeCartesianModified-cartesian_increment-position-2*;
+Img (Velocity)=**/PandaPickCubeCartesianModified-joint-velocity-2*;
+Img (Position Increment)=**/PandaPickCubeCartesianModified-joint_increment-position-2*;
+Img (Torque)=**/PandaPickCubeCartesianModified-joint-torque-2-*"   --smoothing 0.9
+
+python tb_plot.py   --logdir ../logs --title "Success Rate" --yaxis "Success"  --out figs_pub/img   --tags "episode/success"   --groups "\
+Img (Cartesian Increment)=**/PandaPickCubeCartesianModified-cartesian_increment-position-2*;
+Img (Velocity)=**/PandaPickCubeCartesianModified-joint-velocity-2*;
+Img (Position Increment)=**/PandaPickCubeCartesianModified-joint_increment-position-2*;
+Img (Torque)=**/PandaPickCubeCartesianModified-joint-torque-2*"   --smoothing 0.9
 
