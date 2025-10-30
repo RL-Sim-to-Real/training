63f16869ee62ec0118ae8c8727dd8df59c29f664
Alireza Azimi <sazimi@ualberta.ca>
2025-10-29 18:17:13 -0600

    unified message

--- sim_to_real_analysis/real_position_analysis.py
+++ sim_to_real_analysis/real_position_analysis.py
@@ -11,14 +11,15 @@ if __name__ == "__main__":
     env = FrankaPickCubeCartesian(camera_index=0, control_mode="joint_position") 
     env.reset()
     print("Environment reset complete.")
-    controller = env.cmi.current_controller
-    env.cmi.stop_controller(controller)
-    while env.cmi.is_running(controller):
-        print('waiting for controller to stop')
-        time.sleep(1)
-    env.cmi.start_controller(controller)
+
     obs = env.get_state()
     q_pos = obs["joints"]
+    # ensure first joint is at zero
+    q_pos[0] = 0.0
+    print(q_pos)
+    for _ in range(100):
+        env.robot.set_joint_positions(dict(zip(env.joint_names, q_pos)))
+        env.rate.sleep()
     q_pos[0] += 0.5
     for _ in range(100):
         env.robot.set_joint_positions(dict(zip(env.joint_names, q_pos)))
